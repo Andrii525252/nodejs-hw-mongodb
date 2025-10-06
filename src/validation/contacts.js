a9215e54de8228ac52c6eca41fcb69e48e0b9a6e
Andrii <simonandrij717@gmail.com>
2025-10-06 17:03:41 +0200

    fix isFavourite

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,13 +4,13 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(6).max(20).required(),
   email: Joi.string().email().required(),
-  isFavorite: Joi.boolean().required(),
+  isFavourite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(6).max(16),
   email: Joi.string().email(),
-  isFavorite: Joi.boolean(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
