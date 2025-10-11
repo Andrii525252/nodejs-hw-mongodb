24cef3e2375daebb8e535df299d33ddb5c620b5f
Andrii <simonandrij717@gmail.com>
2025-10-11 18:33:53 +0200

    add register route

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFountHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
+import router from './routers/index.js';
 
 const PORT = Number(getEnvVar('PORT'));
 
 export const setupServer = () => {
   const app = express();
+  app.use(cookieParser());
 
   app.use(express.json());
   app.use(cors());
@@ -22,7 +24,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use(errorHandler);
 
