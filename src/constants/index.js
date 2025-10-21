720958884a171430db88cccdaede3aa48a46d7b8
Andrii <simonandrij717@gmail.com>
2025-10-21 17:19:14 +0200

    fix cloudinary tenses

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,7 +27,7 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd());
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
+  CLOUD_NAME: 'dtykafngo',
+  API_KEY: '518697419396435',
+  API_SECRET: 'L2zEqdHj4hpnwpu3yUA9zp8GP20',
 };
